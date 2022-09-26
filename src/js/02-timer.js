4c96a99056f4b7e8c74939c2ecfad4aeb2192dd0
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-26 22:47:09 +0300

    timer working in console

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,32 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+let dateSelected;
+const startBtnRef = document.querySelector('button[data-start]');
+const daysRef = document.querySelector('span[data-hours]');
+const minutesRef = document.querySelector('span[data-minutes]');
+const secondsRef = document.querySelector('span[data-seconds]');
+
+const timer = flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    dateSelected = selectedDates[0];
+  },
+});
+
+function timerStart() {
+  setInterval(() => {
+    const dateNow = Date.now();
+    const difference = dateSelected - dateNow;
+    console.log(convertMs(difference));
+  }, 1000);
+}
+
+startBtnRef.addEventListener('click', timerStart);
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
