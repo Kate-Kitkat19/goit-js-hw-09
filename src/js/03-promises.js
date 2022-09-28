ffacf268642b0df0d2656020fb502ffaa04327c7
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-28 22:43:02 +0300

    testing promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,21 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+const delay = Math.round(Math.random() * 1000);
+const promise = new Promise((resolve, reject) => {
+  const number = Math.random();
+  setTimeout(() => {
+    if (number > 0.5) {
+      resolve(`It's resolved with the delay of ${delay}!`);
+    } else {
+      reject(`It's rejected with the delay of ${delay}!`);
+    }
+  }, delay);
+});
+
+promise.then(data => console.log(data)).catch(error => console.log(error));
