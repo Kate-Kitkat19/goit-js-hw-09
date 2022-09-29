4b42d11a7877e6e9de44902940dd81ae972d06b0
Kate-Kitkat19 <103567509+Kate-Kitkat19@users.noreply.github.com>
2022-09-29 18:38:43 +0000

    Deploying to gh-pages from @ Kate-Kitkat19/goit-js-hw-09@5207afe6d87b56612ac6efbd54056e3b402a4dcd 🚀

--- 03-promises.d9ee0fe7.js
+++
@@ -1,2 +0,0 @@
-const e=Math.round(1e3*Math.random());new Promise(((o,t)=>{const h=Math.random();setTimeout((()=>{h>.5?o(`It's resolved with the delay of ${e}!`):t(`It's rejected with the delay of ${e}!`)}),e)})).then((e=>console.log(e))).catch((e=>console.log(e)));
-//# sourceMappingURL=03-promises.d9ee0fe7.js.map
--- 03-promises.e576cdc0.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=Math.round(1e3*Math.random());new Promise((function(n,o){var e=Math.random();setTimeout((function(){e>.5?n("It's resolved with the delay of ".concat(t,"!")):o("It's rejected with the delay of ".concat(t,"!"))}),t)})).then((function(t){return console.log(t)})).catch((function(t){return console.log(t)}))}();
-//# sourceMappingURL=03-promises.e576cdc0.js.map
